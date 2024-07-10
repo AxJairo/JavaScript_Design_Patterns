801d1ff9553414d8db1a1af6b10fc12932327897
AxeJairo <jairo.martinsmachado@gmail.com>
2024-07-10 11:34:39 -0300

    update: CatClicker v2.0

--- CatClicker/script.js
+++ CatClicker/script.js
@@ -1,8 +1,21 @@
+var contador = 1;
+
+
+function putName(){
+    const names = ["Grumpy" , "Orange"];
+    document.getElementById("g").innerHTML = names[0];
+    document.getElementById("o").innerHTML = names[1];
+}
+
+function getCats(){
+    const elem = document.getElementsByClassName("cat");
+    Array.from(elem).forEach(function(element) {
+        element.addEventListener('click', count);
+    });
+}
+    
 function count(){
-    var contador = 1;
-    const elem = document.getElementById("cat");
-    elem.addEventListener("click", function(){
     document.getElementById("counter").innerHTML = contador;
     contador ++;
-    }, false);
-};
+}
+
